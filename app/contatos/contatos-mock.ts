989a988a589906d05ba928d51b85b2bd4411e46a
Felipe Claro <felipe.claro654@gmail.com>
2017-01-06 09:06:08 -0200

    Aplicacao contatos Angular2

---
+++ app/contatos/contatos-mock.ts
@@ -0,0 +1,9 @@
+import { Contato } from "./contato.model";
+
+export const CONTATOS: Contato[] =[
+    {id: 1, nome: "Fulano de Tal", email: "fulano@s.com", telefone: "(00) 0000-0000"},
+    {id: 2, nome: "Ciclano", email: "ciclano@s.com", telefone: "(00) 0000-0000"},
+    {id: 3, nome: "Pessoa", email: "pessoa@s.com", telefone: "(00) 0000-0000"},
+    {id: 4, nome: "Pessoa2", email: "pessoa2@s.com", telefone: "(00) 0000-0000"},
+    {id: 5, nome: "Pessoa de Tal", email: "pessoadetal@s.com", telefone: "(00) 0000-0000"}
+];
